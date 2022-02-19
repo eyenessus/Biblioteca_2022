d0299179e25e4d0dfd300089e7f8df94474468a4
Eyenessus <eyenessus.com>
2022-02-19 00:27:54 -0300

    Mensagem de nenhum filtro

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,82 @@
+
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+using Biblioteca.Models;
+using Microsoft.AspNetCore.Http;
+namespace Biblioteca.Controllers
+{
+    public class UsuarioController: Controller
+    {
+        public IActionResult ListaDeUsuario(){
+            List<Usuario> usuarioN = new UsuarioService().ListarTodos();
+            return View(usuarioN);
+        }
+
+        public IActionResult EditarUsuario(int id){
+
+            Usuario user = new UsuarioService().Listar(id);
+            return View(user);
+
+        }
+
+        [HttpPost]
+        public IActionResult EditarUsuario(Usuario user){
+            UsuarioService us = new UsuarioService();
+            us.Editar(user);
+            return RedirectToAction("ListaDeUsuario");
+
+        }
+
+        public IActionResult RegistrarUsuarioS(int id){
+
+
+            
+            return View();
+
+        }
+
+
+        [HttpPost]
+        public IActionResult RegistrarUsuarioS(Usuario novoUsuario){
+
+            UsuarioService us = new UsuarioService();
+            us.Registrar(novoUsuario);
+
+            return RedirectToAction("ListaDeUsuario");
+
+        }
+
+
+
+        public IActionResult Excluir(int id){
+            UsuarioService us = new UsuarioService();
+            us.Excluir(id);
+            return RedirectToAction("ListaDeUsuario");
+
+        }
+
+        public IActionResult Sair(){
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+        public IActionResult NeddAdm(){
+            //atenticacao
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
